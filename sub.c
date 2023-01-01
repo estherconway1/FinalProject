884221fe33605e1b1001ddaef763df8006f5350e
Esther Conway <econway1@mail.yu.edu>
2023-01-01 00:50:30 -0500

    first version of SUB

--- sub.c
+++ sub.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 int main(void){
   //char[9] file = "test1.asm";
-  FILE* inStream = fopen("test1.asm", "r");
+  FILE* inStream = fopen("test2.asm", "r");
   FILE* outStream = fopen("output.asm", "w");
 
   char c;
   char word[100];
   char register1[3];
+  char register2[3];
+  char register3[3];
+
+  
     
   int counter = 0;
   while((c = getchar()) != EOF){
@@ -15,10 +19,20 @@ int main(void){
     if (c == '\n'){
       //printf("%s", word);
       for (int i = 0; i < counter; i ++){
-	if (word[i] == 'R' && word[i + 1] == 'S' && word[i+2] == 'T'){
+	if (word[i] == 'S' && word[i + 1] == 'U' && word[i+2] == 'B'){
 	  register1[0] = word[i+4];
 	  register1[1] = word[i+5];
-	  fprintf(outStream, "%s%s%s%s%s", "AND ", register1, ", ", register1, ", #0");  
+
+	  register2[0] = word[i+6];
+	  register2[1] = word[i+7];
+
+	  register3[0] = word[i+9];
+	  register3[1] = word[i+10];
+	  
+	  fprintf(outStream, "%s%s%s%s\n", "NOT\t", register2, ", ", register2);
+	  fprintf(outStream, "\t%s%s%s%s%s\n", "ADD ", register2, ", ", register2, ", #1");
+	  fprintf(outStream, "\t%s%s%s%s%s%s", "ADD ", register1, ", ", register2, ", ", register3);
+	  
 	  break;
 	}
 	fprintf(outStream, "%c", word[i]);
