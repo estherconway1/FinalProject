a67ebfae9a05f77d384e80a50700af614706fff0
Esther Conway <econway1@mail.yu.edu>
2022-12-31 23:21:01 -0500

    not working

--- rst.c
+++ rst.c
@@ -8,33 +8,40 @@ int main(void){
 
   char check[2]; 
 
-  char word[7];
+  char word[10];
+
+  char reg[3];
+
+  
 
   char code[4] = "RST";
   
   int counter = 0;
   while((c = getc(inStream)) != EOF){
-    
-    char *ptr = &c;
-    //fprintf(outStream, "%c", *ptr);
+    //printf("%c", c);
     
     word[counter] = c;
 
-    if (((*ptr) == ' ' || ((*ptr) == '\n') || ((*ptr) == '\t'))){
-      for(int i=0; i < counter; i++){
-	fprintf(outStream, "%c", word[counter]);
-	printf("%d", word[counter]);
-      }
-      counter = 0;
+    if ((c == '\n')){
+      if (word[0] == 'R' && word[1] == 'S' && word[2] == 'T'){
+	printf("%s", word);
+	fprintf(outStream, "%s", "AND");
+	//fprintf(outStream, "%c", word[4]);
+	//fprintf(outStream, "%c", word[5]);
+	//fprintf(outStream, "%s", ", #0\n");
+	//counter = 0;
+	}
+      //else{
+      // for(int i = 0; i <= counter; i++){
+      //     fprintf(outStream, "%c", word[i]);
+    counter = 0;
     }
-
     counter ++;
     
-      
+    }
       
     
-     
-  }
+ 
 
     
 
