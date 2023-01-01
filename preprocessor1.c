5ed41a17ed3fc373a8e65e56fc23d59868603ba8
Esther Conway <econway1@mail.yu.edu>
2023-01-01 13:56:19 -0500

    super annoying version that tries to check for errors but is just messy

--- preprocessor1.c
+++ preprocessor1.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
 int main(void){
-  //char[9] file = "test1.asm";
+  //Open the input and output streams
   FILE* inStream = fopen("test2.asm", "r");
   FILE* outStream = fopen("output.asm", "w");
 
-  char c;
-  char word[100];
-  char register1[3];
-  char register2[3];
-  char register3[3];
+  char c;            // Holds the current character from the input stream
+  char word[100];    // Holds the current line
+  char register1[3]; // Holds the 1st operand in the instructions we're editing
+  char register2[3]; // Holds the 2nd operand
+  char register3[3]; // Holds the 3rd operand
+  int lineWritten = 0;
 
   
     
-  int counter = 0;
+  int counter = 0; //Holds the possition that we are up to in the array that holds the line (word[])
   while((c = getchar()) != EOF){
+    //Add the current character to the array
     word[counter] = c;
 
+    //If the character is a newline, check if it needs to be changed
     if (c == '\n'){
       //printf("%s", word);
       for (int i = 0; i < counter; i ++){
 	
-	//RST
-	if (word[i] == 'R' && word[i + 1] == 'S' && word[i+2] == 'T'){
+	//If it encounters a RST opcode edit to for the instuctions to reset the  register (AND register with #0)
+	if (word[i] == 'R' && word[i + 1] == 'S' && word[i+2] == 'T' && lineWritten == 0){
+	  lineWritten = 1;
 	  int offset = i + 2;
-	  while(word[offset] != 'R'){
+	  
+	  while(word[offset] != 'R' && word[offset] != '\n'){
 	    offset ++;
 	  }
+
+	  if ((register1[0] != 'R') || (register1[1] != '0' && register1[1] != '1' && register1[1] != '2' && register1[1] != '3' && register1[1] != '4' && register1[1] != '5' && register1[1] != '6' && register1[1] != '7')){
+	     fprintf(stderr, "ERROR: OPERAND SHOULD BE A REGISTER\n");
+	    
+	    }
+	   
 	  register1[0] = word[offset];
 	  register1[1] = word[offset + 1];
 	  word[offset + 1];
@@ -34,8 +45,10 @@ int main(void){
 	  break;
 	}
 
-	//SET
-	if (word[i] == 'S' && word[i + 1] == 'E' && word[i+2] == 'T'){
+	//SET a register - more complicated than originally thought because operand can be an number of variable length
+	if (word[i] == 'S' && word[i + 1] == 'E' && word[i+2] == 'T' && lineWritten == 0){
+	  lineWritten = 1;
+	  
 	  int offset = i + 2;
 	  while(word[offset] != 'R'){
 	    offset ++;
@@ -44,21 +57,35 @@ int main(void){
 	  register1[1] = word[offset + 1];
 	  offset++;
 
-	   while(word[offset] != '#'){
+	  if ((offset > counter) || (register1[0] != 'R') || (register1[1] != '0' && register1[1] != '1' && register1[1] != '2' && register1[1] != '3' && register1[1] != '4' && register1[1] != '5' && register1[1] != '6' && register1[1] != '7')){
+	     fprintf(stderr, "ERROR: OPERAND SHOULD BE A REGISTER\n");
+	     //break;
+	    }
+
+	   while(word[offset] != '#' && word[offset] != '\n'){
 	    offset ++;
-	  }
-	  register2[0] = word[offset];
-	  register2[1] = word[offset + 1];
-	   
+	   }
+
+	    register2[0] = word[offset];
+	    register2[1] = word[offset + 1];
+	    offset++;
+
+	    if ((offset > counter) || (register2[0] != '#') || (register2[1] != '0' && register2[1] != '1' && register2[1] != '2' && register2[1] != '3' && register2[1] != '4' && register1[1] != '5' && register2[1] != '6' && register2[1] != '7')){
+	     fprintf(stderr, "ERROR: OPERAND SHOULD BE A NUMBER\n");
+	    break;
+	    }
+
+	  	   
 	  word[offset + 1];
-	  fprintf(outStream, "%s%s%s%s%s", "AND\t", register1, ", ", register1, ", #0");
-	  fprintf(outStream, "%s%s%s%s%s", "ADD\t", register1, ", ", register1, ", ",  register2);
+	  fprintf(outStream, "%s%s%s%s%s\n", "AND\t", register1, ", ", register1, ", #0");
+	  fprintf(outStream, "\t%s%s%s%s%s%s", "ADD\t", register1, ", ", register1, ", ",  register2);
 	  register1[1] = ' ';
 	  break;
 	}
 
-	//SUB
-	if (word[i] == 'S' && word[i + 1] == 'U' && word[i+2] == 'B'){
+	//SUB two registers or a register and a decimal number
+	if (word[i] == 'S' && word[i + 1] == 'U' && word[i+2] == 'B' && lineWritten == 0){
+	  lineWritten = 1;
 	  int offset = i + 2;
 	  while(word[offset] != 'R'){
 	    offset ++;
@@ -89,8 +116,8 @@ int main(void){
 	  break;
 	}
 
-	//I should add an OPCODE that sets the registers
 	fprintf(outStream, "%c", word[i]);
+      }
     }
       fprintf(outStream, "%c", '\n');
       counter = -1;
