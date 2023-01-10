6f285e30db68d3788db66fd21450e59778e2bb26
Esther Conway <econway1@mail.yu.edu>
2023-01-09 19:28:24 -0500

    continued organizing the code

--- organizedCode.c
+++ organizedCode.c
@@ -1,22 +1,59 @@
-void findOpcodes(line, lineSize);
 
+void RST();
+void SET();
+void SUB();
+void MLT();
+
+
+void findOpcodes(char[] line, int lineSize){
+
+  //Iterate over the line
+  for (int i = 0; i < lineSize; i ++){
+    
+    
+    if (line[i] == 'R' && line[i + 1] == 'S' && line[i+2] == 'T'){
+      
+      RST(line, i);
+    }
+    
+
+    if (line[i] == 'S' && line[i + 1] == 'E' && line[i + 2] == 'T'){
+      SET(line, i);
+    }
+    
+
+    if (line[i] == 'S' && line[i + 1] == 'U' && line[i+2] == 'B'){
+      SUB(line, i);
+    }
+
+    
+    if (line[i] == 'M' && line[i + 1] == 'L' && line[i+2] == 'T'){
+      MLT(line, i);
+    }
+
+      
+  }
+
+  
+
+
+  
+}
 int main(void){
 	FILE* inStream = fopen("test2.asm", "r");
  	FILE* outStream = fopen("output.asm", "w");
 	
 	char line[50];
-
-	int counter = 0;
+	int linePosition = 0;
+	
 	while((c = getchar()) != EOF){
-		line[counter] = c;
-		if (c == '\n'){
-			findOpcodes(word, counter);
-			fprintf(outStream, "%c", '\n');
-			counter = -1
-
-		}
-		counter++;
-
+	  line[counter] = c;
+	  if (c == '\n'){
+	    findOpcodes(line, linePosition);
+	    fprintf(outStream, "%c", '\n');
+	    linePosition = -1
+	      }
+	  counter++;
 	}
 }
 
