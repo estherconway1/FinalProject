42b933af54d96b0fb7c56845cf4626bb2278510e
Esther Conway <econway1@mail.yu.edu>
2023-01-16 00:04:26 -0500

    MLT working

--- organizedCode.c
+++ organizedCode.c
@@ -2,9 +2,19 @@
 
 FILE* outStream;
 
+int getOperandSize(char line[], int position){
+  int size = 0;
+  while (line[position] != '\n' && line[position] != '\t' && line[position] != ' '){
+    position++;
+    size++;
+  }
+  return size;
+}
+
+
 int RST(char line[], int position){
   
-  char register1[3];
+  char register1[3] = "  ";
   
   while(line[position] != 'R' && line[position] != '\n'){
     position++;
@@ -20,12 +30,86 @@ int RST(char line[], int position){
 
 }
 int SET(char line[], int position){
+  char register1[3] = "  ";
+
+   while(line[position] != 'R'){
+    position++;
+   }
+
+  register1[0] = line[position];
+  position++;
+  register1[1] = line[position];
+
+  while(line[position] != 'x' && line[position] != '#'){
+    position++;
+   }
+
+  int operandSize = getOperandSize(line, position);
+
+  char operand2[operandSize + 1] = "  ";
+
+  for (int i = 0; i < operandSize - 2; i++){
+    operand2[i] = line[position];
+    position++;
+  }
+
+ fprintf(outStream, "\t%s\t%s, %s, %s\n", "AND", register1, register1, "#0");
+ fprintf(outStream, "\t%s\t%s, %s, %s", "ADD", register1, register1, operand2);
+  
+
+  
+  
   return 1;
 }
 int SUB(char line[], int position){
   return 1;
 }
 int MLT(char line[], int position){
+  char register1[3] = "  ";
+  char register2[3] = "  ";
+  char register3[3] = "  ";
+
+  while(line[position] != 'R'){
+    position++;
+ }
+
+  register1[0] = line[position];
+  position++;
+  register1[1] = line[position];
+
+  while(line[position] != 'R'){
+    position++;
+ }
+
+  register2[0] = line[position];
+  position++;
+  register2[1] = line[position];
+
+  while(line[position] != 'R'){
+    position++;
+ }
+
+  register3[0] = line[position];
+  position++;
+  register3[1] = line[position];
+
+ //Print out the new instructions. Which are:
+ //Allocate a memory location
+ fprintf(outStream, "\t%s\t%s\n", ".FILL", "#0");
+ //Save the contents of the second operand register at that location
+ fprintf(outStream, "\t%s\t%s%s\n", "ST", register2, ", #-2");
+ //Clear the destination register
+ fprintf(outStream, "\t%s\t%s%s%s%s\n", "AND", register1, ", ", register1, 
+", #0");
+ 
+ //Repeatedly add, and decrement the register until it is 0
+ fprintf(outStream, "\t%s\t%s%s%s%s%s\n", "ADD", register1, ", ", 
+register1, ", ", register3);
+ fprintf(outStream, "\t%s\t%s%s%s%s\n", "ADD", register2, ", ", register2, 
+", #-1");
+ fprintf(outStream, "\t%s\t%s\n", "BRp", "#-3");
+ //Restore the contents of the counter register
+ fprintf(outStream, "\t%s\t%s%s\n", "LD", register2, ", #-7"); 
   return 1;
 }
 
@@ -56,7 +140,7 @@ int findOpcodes(char line[], int lineSize){
 
     
     if (line[i] == 'M' && line[i + 1] == 'L' && line[i+2] == 'T'){
-      MLT(line, i);
+      MLT(line, i + 2);
       return 4;
     }
 
